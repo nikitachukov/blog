891f43daf04ecc8bcd431bdcedf97c4966e4b12d
nikitos <nikitachukov@me.com>
2014-03-25 19:38:22 +0400

    Окончание второго урока

--- firstapp/article/urls.py
+++ firstapp/article/urls.py
@@ -12,5 +12,7 @@ urlpatterns = patterns('',
     # url(r'^2/', 'article.views.template_two'),
     # url(r'^3/', 'article.views.template_three_simple'),
     url(r'^articles/all/$', 'article.views.articles'),
-    # url(r'^articles/get/(?<article_id>)\d+/$', 'article.views.article'),
+    # url(r'^articles/get/(?P<article_id>)\/$', 'article.views.article'),
+    url(r'^articles/get/(?P<article_id>\d+)/$', 'article.views.article'),
+    #url(r'^articles/get/(?<article_id>)\d+/$', 'article.views.article'),
 )
--- firstapp/article/views.py
+++ firstapp/article/views.py
@@ -21,8 +21,8 @@ def template_three_simple(request):
     view='template three'
     return render_to_response('myview.html',{'name':view})
 
-def articles(request):
+def articles(request, article_id=1):
     return render_to_response('articles.html',{'articles':Article.objects.all()})
 
-def article(request, article_id=1):
+def article(request,article_id=1 ):
     return render_to_response('article.html',{'article':Article.objects.get(id=article_id),'comments':Comments.objects.filter(comments_article_id=article_id)})
