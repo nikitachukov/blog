adb0837fca39da9da923741698b028f8cd418fa8
nikitos <nikitachukov@me.com>
2014-03-25 20:08:06 +0400

    Пытался исправить имена моделей

--- firstapp/article/models.py
+++ firstapp/article/models.py
@@ -1,9 +1,13 @@
+ # -*- coding: utf-8 -*-
 from django.db import models
 
 # Create your models here.
 class Article(models.Model):
 	class Meta():
 		db_table="article"
+        verbose_name='Статья'
+        verbose_name_plural='Статьи'
+
 	article_title=models.CharField(max_length=200)
 	article_text=models.TextField()
 	article_date=models.DateTimeField()
@@ -12,6 +16,7 @@ class Article(models.Model):
 class Comments(models.Model):
 	class Meta():
 		db_table="comments"
+        verbose_name='Коментарии'
 	comments_text=models.TextField()
 	comments_article = models.ForeignKey(Article)
 
