568d11b2057c4f11d81e9a5c8ca66c07d6d1039a
Nikita Chukov <nikitachukov@me.com>
2014-03-26 02:58:10 +0400

    add css

--- firstapp/article/urls.py
+++ firstapp/article/urls.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 __author__ = 'nikitos'
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'firstapp.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    # url(r'^1/', 'article.views.basic_one'),
-    # url(r'^2/', 'article.views.template_two'),
-    # url(r'^3/', 'article.views.template_three_simple'),
-
     url(r'^articles/all/$', 'article.views.articles'),
     url(r'^articles/get/(?P<article_id>\d+)/$', 'article.views.article'),
     url(r'^', 'article.views.articles'),
