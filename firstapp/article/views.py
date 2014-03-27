aaae527d0ba4e97b4f4102967b7f34e0da128f15
nikitos <nikitachukov@me.com>
2014-03-27 17:33:46 +0400

    урок 9

--- firstapp/article/views.py
+++ firstapp/article/views.py
@@ -11,9 +11,15 @@ def articles(request):
 
 def addlike(request,article_id):
     try:
-        article=Article.objects.get(id=article_id)
-        article.article_likes+=1
-        article.save()
+        if article_id in request.COOKIES:
+            redirect('/')
+        else:
+            article=Article.objects.get(id=article_id)
+            article.article_likes+=1
+            article.save()
+            response=redirect('/')
+            response.set_cookie(article_id,'test')
+            return response
     except ObjectDoesNotExist:
         raise Http404
     return redirect('/')
@@ -28,10 +34,12 @@ def article(request,article_id):
     return render_to_response('article.html',args)
 
 def addcomment(request,article_id):
-     if request.POST:
+     if request.POST and ("pause" not in request.session):
          form = CommentForm(request.POST)
          if form.is_valid():
              comment=form.save(commit=False)
              comment.comments_article=Article.objects.get(id=article_id)
              form.save()
+             request.session.set_expiry(60)
+             request.session['pause']=True
      return redirect('/articles/get/%s/'%article_id)
